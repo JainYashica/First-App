1d1effb63cb58a0045b47bf0e319d9833f2c24f0
ShashiRanjan1 <shashiranjan2310@gmail.com>
2016-11-22 02:58:52 +0530

    added

--- public/Controller/controller.js
+++ public/Controller/controller.js
@@ -1,4 +1,5 @@
-function AppCtrl(){
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("Hello World from controller")
 
-console.log("its calling ")
-}
+}]);
